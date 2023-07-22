34e2bd2d2ef6787898be75641dfc74aab1df0466
Mohammed Kayser <104914864+mohammedkayser@users.noreply.github.com>
2023-07-22 20:24:54 +0530

    edited contact form

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,3 +1,23 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const sendMessageButton = document.getElementById("sendMessageButton");
+
+  sendMessageButton.addEventListener("click", function () {
+    const form = document.getElementById("contactForm");
+    const formData = new FormData(form);
+
+    const name = formData.get("name");
+    const email = formData.get("email");
+    const phone = formData.get("phone");
+    const subject = formData.get("subject");
+    const message = formData.get("message");
+
+    const mailtoLink = `mailto:mohammedkayser143@gmail.com?subject=${encodeURIComponent(
+      subject
+    )}&body=${encodeURIComponent(message)}`;
+    
+    window.location.href = mailtoLink;
+  });
+});
 
 
 
